065823842fc18179981e5ea4f3ed6a8e0c416a21
Miguel Ángel Vílchez López <mangel@matronica.eu>
2024-01-26 21:33:11 +0100

    Very simple earn begining

---
+++ lib/binance/spot/simple_earn.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Binance
+  class Spot
+    # all wallet endpoints
+    # @see https://binance-docs.github.io/apidocs/spot/en/#wallet-endpoints
+    module SimpleEarn
+
+      # Account Status (System)
+      #
+      # GET /sapi/v1/system/status
+      #
+      # Fetch account status.
+      #
+      # @see https://binance-docs.github.io/apidocs/spot/en/#simple-account-user_data
+      def user_data
+        @session.public_request(path: '/sapi/v1/simple-earn/account')
+      end
+
+    
+    end
+  end
+end
