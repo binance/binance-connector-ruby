d8f0e9bb2c3b4e038eb56b947f7ea8dbc04c0cd6
Miguel Ángel Vílchez López <mangel@matronica.eu>
2024-01-26 21:50:19 +0100

    bug fixed

--- lib/binance/spot/simple_earn.rb
+++ lib/binance/spot/simple_earn.rb
@@ -6,17 +6,18 @@ module Binance
     # @see https://binance-docs.github.io/apidocs/spot/en/#wallet-endpoints
     module SimpleEarn
 
-      # Account Status (System)
+      # All Coins' Information on 'simple earn' (USER_DATA)
       #
-      # GET /sapi/v1/system/status
+      # GET /sapi/v1/simple-earn/account'
       #
-      # Fetch account status.
+      # Get information of coins (in simple earn) for user.
       #
+      # @param kwargs [Hash]
+      # @option kwargs [Integer] :recvWindow The value cannot be greater than 60000
       # @see https://binance-docs.github.io/apidocs/spot/en/#simple-account-user_data
-      def simple_earn_account
-        @session.public_request(path: '/sapi/v1/simple-earn/account')
+      def simple_earn_account(**kwargs)
+        @session.sign_request(:get, '/sapi/v1/simple-earn/account', params: kwargs)
       end
-
     
     end
   end
